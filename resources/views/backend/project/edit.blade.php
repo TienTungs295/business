77eb6848196340b65d9ef9dfe399bd211611c56b
tienlq <tientungs295@gmail.com>
2023-03-05 23:12:33 +0700

    update be page

--- resources/views/backend/project/edit.blade.php
+++ resources/views/backend/project/edit.blade.php
@@ -32,7 +32,7 @@
                                     <div class="col-md-3">
                                         <label class="form-label">Độ ưu tiên</label>
                                         <input type="number"
-                                               value="{!! old('priority', isset($post_category->priority) ? $post_category->priority : '')!!}"
+                                               value="{!! old('priority', isset($project_category->priority) ? $project_category->priority : '')!!}"
                                                class="form-control" name="priority" min="0">
                                     </div>
                                 </div>
